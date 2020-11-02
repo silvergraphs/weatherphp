2cd06af98263d2e513068a5104a6d42224e6e49b
Bruno Caruso <brunofcaruso@gmail.com>
2020-11-02 16:08:07 -0300

    Mensaje de error
    Se agrego un mensaje de error cuando la ciudad a buscar no existe

--- index.php
+++ index.php
@@ -42,7 +42,8 @@ if ($_POST['city']) {
     </form>
 <hr/>
 
-    <?php if ($_POST['city']) {
+    <?php
+    if ($_POST['city'] && $apiData) {
       echo '<h3>' .
         ucwords($_POST['city']) .
         ', ' .
@@ -73,7 +74,13 @@ if ($_POST['city']) {
         '%</p>';
 
       echo "       <hr>";
-    } ?>
+    }
+    if ($apiData === false) {
+      echo '<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-exclamation-circle-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+      <path fill-rule="evenodd" d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
+    </svg> City not found<hr>';
+    }
+    ?>
 
     
         
